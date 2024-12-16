13841381eca768d39d93b7cfec15c47814b49836
sahilrush <sahilrevankar20co21@gmail.com>
2024-12-16 06:57:42 +0530

    v1 completed

--- src/lib/rag-chat.ts
+++ src/lib/rag-chat.ts
@@ -3,7 +3,7 @@ import { redis } from "./redis";
 
 export const ragChat = new RAGChat({
   model: openai("gpt-3.5-turbo", {
-    "apiKey": "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"
+    "apiKey": process.env.OPENAI_API_KEY,
         }),
         redis:redis
   })
